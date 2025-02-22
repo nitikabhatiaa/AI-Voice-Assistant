db119b77d89e380c5759a99133d28734449761aa
Nitika <nitikabhatia205@gmail.com>
2025-02-22 23:02:17 +0530

    Fix render-build.sh script

--- render-build.sh
+++ render-build.sh
@@ -1,1 +1,10 @@
-"" 
+#!/bin/bash
+
+# Make script executable (in case it's needed)
+chmod +x render-build.sh
+
+# Install espeak
+apt-get update && apt-get install -y espeak
+
+# Install dependencies
+pip install -r requirements.txt
